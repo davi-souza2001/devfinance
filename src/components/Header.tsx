383b568aea1eb61c52e754b9ae46529eaa864b92
Davi Souza <davisouza2001dv@gmail.com>
2023-03-26 19:55:24 -0300

    up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Logo from '../../public/Logo.svg'
 export function Header() {
     return (
         <div className='h-16 w-full bg-red-500 flex items-center justify-start'>
-            <div className='h-full w-40 bg-yellow-500 flex items-center justify-around'>
+            <div className='h-full w-40 bg-yellow-500 flex items-center justify-around text-lg'>
                 <Image alt='Logo' src={Logo} />
                 <p>Devfinance</p>
             </div>
