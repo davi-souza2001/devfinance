154306afc53f5c08d6a117db39068827813186df
Davi Souza <davisouza2001dv@gmail.com>
2023-04-05 15:18:52 -0300

    add menu header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
+'use client'
+
 import { Header } from '@/components/Header'
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+
 import './globals.css'
 
 interface RootLayoutProps {
@@ -8,10 +13,14 @@ interface RootLayoutProps {
 export default function RootLayout(props: RootLayoutProps) {
 	return (
 		<html>
-				<body className='font-poppins'>
-					<Header />
-					{props.children}
-				</body>
+			<body className='font-poppins'>
+				<CacheProvider>
+					<ChakraProvider>
+						<Header />
+						{props.children}
+					</ChakraProvider>
+				</CacheProvider>
+			</body>
 		</html>
 	)
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,29 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import Image from 'next/image'
 
 import Logo from '../../public/Logo.svg'
 
 export function Header() {
-    return (
-        <div className='h-16 w-full bg-red-500 flex items-center justify-start'>
-            <div className='h-full w-40 bg-yellow-500 flex items-center justify-around text-lg'>
-                <Image alt='Logo' src={Logo} />
-                <p>Devfinance</p>
-            </div>
-        </div>
-    )
+	return (
+		<div className='h-16 w-full bg-red-500 flex items-center justify-between'>
+			<div className='h-full w-40 bg-yellow-500 flex items-center justify-around text-lg'>
+				<Image alt='Logo' src={Logo} />
+				<p>Devfinance</p>
+			</div>
+			<div className='h-full w-40 bg-yellow-300 flex items-center justify-end'>
+				<Menu>
+					<MenuButton as={Button} >
+						Actions
+					</MenuButton>
+					<MenuList>
+						<MenuItem>Download</MenuItem>
+						<MenuItem>Create a Copy</MenuItem>
+						<MenuItem>Mark as Draft</MenuItem>
+						<MenuItem>Delete</MenuItem>
+						<MenuItem>Attend a Workshop</MenuItem>
+					</MenuList>
+				</Menu>
+			</div>
+		</div>
+	)
 }
