6d59c1371cbe3cbba7c63e35dd5a0c5799dc6dec
Davi Souza <davisouza2001dv@gmail.com>
2023-07-07 11:48:47 -0300

    fix the auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,11 @@ import { NextResponse, NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
 	const token = request.cookies.get('tokenAuthFinance')?.value
-	console.log('asdasd')
-	if (!token) {
+	if (!token && request.nextUrl.pathname !== '/login') {
 		return NextResponse.redirect(new URL('/login', request.url))
 	}
 }
 
 export const config = {
-	matcher: '/account',
+	matcher: '/',
 }
